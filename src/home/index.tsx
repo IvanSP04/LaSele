a43d6a06135bdf6b92674da167f875839ee3048b
IvanSP04 <ivan.sterlingp@uniagustiniana.edu.co>
2026-04-10 19:20:18 +0000

    Avances en clase 10

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router'
 
 interface Ranking {
   rank: number
@@ -7,10 +8,12 @@ interface Ranking {
   matchesPlayed: number
 }
 
+type FiltroTipo = 'posiciones' | 'goleador' | 'asistencias' | 'amarillas' | 'atajadas'
+
 function Home() {
   const [ranking, setRanking] = useState<Ranking[]>([])
   const [title, setTitle] = useState('')
-
+  const [filtro, setFiltro] = useState<FiltroTipo>('posiciones')
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -21,13 +24,47 @@ function Home() {
         setTitle(data.standings[0].competitionName)
       } catch (error) {
         console.error('Error cargando datos:', error)
-      }
+     }
     }
 
     fetchData()
   }, [])
+  
+  const equiposMap: Record<string, string> = {
+  "América de Cali SA": "america-de-cali",
+  "AD Pasto": "Pasto",
+  "CA Bucaramanga": "atletico-bucaramanga",
+  "Club Atlético Nacional SA": "atletico-nacional",
+  "Club Deportes Tolima SA": "deportes-tolima",
+  "Asociación Deportivo Cali": "deportivo-cali",
+  "Deportivo Independiente Medellín": "independiente-medellin",
+  "Club Independiente Santa Fe": "independiente-santa-fe",
+  "CD Popular Junior FC SA": "junior",
+  "Millonarios FC": "millonarios",
+  "Once Caldas SA": "once-caldas",
 
+  "Internacional de Bogotá": "internacional-bogota",
+  "Club Llaneros SA": "llaneros",
+  "Águilas Doradas": "aguilas-doradas",
+  "Fortaleza FC": "fortaleza",
+  "Alianza FC": "alianza",
+  "Jaguares de Córdoba FC": "jaguares",
+  "Cúcuta Deportivo FC": "cucuta",
+  "Boyacá Chicó FC": "boyaca-chico",
+  "Deportivo Pereira FC": "pereira"
+};
   return (
+  
+      <div className="filtros">
+        {filtros.map((onestat) => (
+          <button
+            key={onestat}
+            onClick={() => setFiltro(onestat)}
+            className={filtro === onestat ? 'activo' : ''}
+          >
+            {onestat}
+          </button>
+        ))
     <div className="tabla-container">
       <h2>{title}</h2>
       <table className="tabla-posiciones">
@@ -43,7 +80,11 @@ function Home() {
           {ranking.map((equipo) => (
             <tr key={equipo.rank}>
               <td>{equipo.rank}</td>
-              <td>{equipo.contestantName}</td>
+              <td>
+                <Link to={`/equipo/${equiposMap[equipo.contestantName] || "default"}`}>
+                {equipo.contestantName}
+                </Link>
+                </td>
               <td>{equipo.matchesPlayed}</td>
               <td>{equipo.points}</td>
             </tr>
